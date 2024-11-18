eb520935b7bfa62482e88dcd9534d64a8a62cd72
Daniel Schiavini <daniel.schiavini@gmail.com>
2024-11-18 10:12:28 +0100

    chore: update era test node

--- boa_zksync/util.py
+++ boa_zksync/util.py
@@ -58,7 +58,7 @@ def install_zkvyper_compiler(
 
 
 def install_era_test_node(
-    source="https://github.com/matter-labs/era-test-node/releases/download/v0.1.0-alpha.30/era_test_node-v0.1.0-alpha.30-x86_64-unknown-linux-gnu.tar.gz",  # noqa: E501
+    source="https://github.com/matter-labs/era-test-node/releases/download/v0.1.0-alpha.32/era_test_node-v0.1.0-alpha.32-x86_64-unknown-linux-gnu.tar.gz",  # noqa: E501
     destination="/usr/local/bin/era_test_node",
 ):
     """
